42958ac8f13a08b58baf101064058e93e2f89d85
Svilen Popov <svil4ok@gmail.com>
2025-11-23 18:09:09 +0200

    chore: configured husky and pre commit tasks

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+// Setting this as a .js file will allow us to run commands that are relevant
+// to a file (e.g test using the correct config for that file) in future
+
+const lintstagedConfig = {
+  "*": [
+    "eslint --no-warn-ignored --flag unstable_ts_config",
+    "dotenvx run -- vitest related --run --no-coverage",
+  ],
+};
+
+export default lintstagedConfig;
