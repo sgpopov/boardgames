8049dd28f2f31ab230df364bc6dfc46c0176f936
Svilen Popov <svil4ok@gmail.com>
2025-11-23 14:10:08 +0200

    ci: adjust build settings

--- next.config.js
+++ next.config.js
@@ -2,11 +2,36 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  assetPrefix: "/public",
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
   output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/boardgames",
+
+  /**
+   * Specify a name to use for a custom build directory to use instead of .next.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/distDir
+   */
   distDir: "build/public",
-  // https://nextjs.org/docs/messages/export-image-api
-  images: { unoptimized: true },
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
